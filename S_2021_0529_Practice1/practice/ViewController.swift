79c272e9ba9965135c835960bbc760b771dbefa1
RuiOkazaki <okazaki200226@gmail.com>
2021-05-29 22:33:24 +0900

    練習用のアプリを作成した

---
+++ S_2021_0529_Practice1/practice/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by 岡崎流依 on 2021/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
